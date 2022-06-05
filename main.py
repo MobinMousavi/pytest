df2a89163ca97b60ee40fcf3ebfde6b0902f6a61
Mobin Mousavi <mousavimobin1380@gmail.com>
2022-06-05 19:40:47 +0430

    Upgrade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 class Stack:
-    CAPACITY: int = 10_000_000
+    CAPACITY: int = 10
 
     def __init__(self) -> None:
         self.__items = []
--- test_stack.py
+++ test_stack.py
@@ -16,3 +16,37 @@ def test_push_one_item():
     assert st.top() == 2
     assert repr(st) == "Stack()"
     assert str(st) == "Stack(2)"
+
+def test_push_multiple_items():
+    st = Stack()
+    st.push(3)
+    st.push(5)
+    st.push(7)
+    assert len(st)==3
+    assert st.top()==7
+    assert str(st)=="Stack(3, 5, 7)"
+
+def test_push_multiple_items_until_exception_raises():
+    st = Stack()
+    for i in range(10):
+        st.push(i)
+    assert st.is_full()
+    with pytest.raises(OverflowError):
+        st.push(23)
+
+def test_pop_one_item():
+    st = Stack()
+    st.push(5)
+    top = st.pop()
+    assert top == 5
+    assert st.is_empty()
+    assert len(st) == 0
+
+def test_pop_multiple_items():
+    st = Stack()
+    st.push(3)
+    st.push(7)
+    assert st.pop() == 7
+    assert st.pop() == 3
+    with pytest.raises(IndexError):
+        st.pop()
